d91e6f72edc8f33679a660e106c9826ddb25c8c5
jieyintan <51181712+jieyintan@users.noreply.github.com>
2020-02-23 03:41:10 +0800

    pokemon

--- project/js/main.js
+++ project/js/main.js
@@ -1,11 +1,46 @@
+// P.getPokemonByName('eevee') // with Promise
+//     .then(function(response) {
+//       console.log(response);
+//     });
 
-function myFunction() {
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // jQuery can do a lot of crazy stuff so make sure to google around to find out more
 
-  $("#demo").html("NEWWW PARAGRAPH #javascript #fire");
-
-  // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
+ function refresh(){
+	var pokemonA = Math.floor((Math.random() * 151) + 1);
+	getPokemonA(pokemonA);
 }
 
+ $(document).ready(function (){
+ 	refresh();
+})
+
+function getPokemonA(pokemonA){
+
+	P.getPokemonByName(pokemonA) // with Promise
+    .then(function(response) {
+      console.log(response);
+      
+      // $(".pokemon_ID").text("No."+response.id);
+      // $(".pokemon_NAME").text(response.name);
+      // $(".image").html("<img src='"+response.sprites.front_default+"'>")
+      
+      if (response.types.length === 1) {
+      	$(".pokemon_SPECIES").text(response.types[0].type.name +" Pokémon");
+      } 
+      else {
+      	$(".pokemon_SPECIES").text(response.types[0].type.name +" & "+response.types[1].type.name +" Pokémon");
+      	}
+      
+	  $(".pokemon_HEIGHT").text(response.height*10 +" cm");
+      $(".pokemon_WEIGHT").text(response.weight/10 + ' kg');
+      $(".ability").text(response.abilities[0].ability.name);
+
+      var moveset = Math.floor(Math.random() * response.moves.length);
+      $(".moveset").text(response.moves[moveset].move.name);
+
+    });
+
+}
+
+function checkGuess (pokemonA) {	
+	console.log(pokemonA);
+    };
